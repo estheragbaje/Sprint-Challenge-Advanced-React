3fd7c80a482ab172e0423f06f0ee200fd47a57b8
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-10-04 09:05:00 +0100

    Rendered the NavBar in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import PlayerCard from "./components/PlayerCard";
 import PlayerCard2 from "./components/PlayerCard2";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       {/* <PlayerCard /> */}
+      <NavBar />
       <PlayerCard2 />
     </div>
   );
